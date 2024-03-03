976add31742e0303dc6a84d30c23b8260b434b90
DmitriyKryuchkov <kruchkovdmitriy48@mail.ru>
2024-03-03 14:38:26 +0300

    added 2

--- 1.sh
+++ 1.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-echo "" > first.txt
-date +"%H:%M" >> first.txt
+date +"%H:%M" > first.txt
 date +"%D" >> first.txt
 awk -F: '($3>=1000)&&($1!="nobody")||($1=="root"){print $1}' /etc/passwd >> first.txt
 cat first.txt
